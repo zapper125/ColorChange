71bda09c4fe428fa621ccba2af3bd4f8dfa7536d
Aaron Christiansen <125aaron@gmail.com>
2018-10-09 12:21:07 -0600

    Changedd view folder and colortool

---
+++ ColorChange/Controller/ColorChangeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ColorChange
+//
+//  Created by Christiansen, Aaron on 10/9/18.
+//  Copyright © 2018 Ctec. All rights reserved.
+//
+
+import UIKit
+
+public class ColorChangeViewController: UIViewController
+{
+    public override func viewDidLoad() -> Void
+    {
+    super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ColorChange/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  ColorChange
+//
+//  Created by Christiansen, Aaron on 10/9/18.
+//  Copyright © 2018 Ctec. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
